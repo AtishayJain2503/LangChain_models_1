c6c7639f0a1504e3ee5fdc6cb3c534b8e3c57c5f
AtishayJain2503 <atishayjain2503@gmail.com>
2025-05-31 21:34:28 +0530

    Add files via upload

---
+++ embedding_hf_local.py
@@ -0,0 +1,17 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+import os
+
+os.environ['HF_HOME'] = 'D:/huggingface_cache'
+
+
+embedding = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+
+documents = [
+    "Delhi is the capital of India",
+    "Kolkata is the capital of West Bengal",
+    "Paris is the capital of France"
+]
+
+vector = embedding.embed_documents(documents)
+
+print(str(vector))
